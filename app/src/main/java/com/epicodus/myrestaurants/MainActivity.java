e6c0d9b076cd2b554c67ad30e3a90ad4230f813d
Courtney <courtney@epicodus.com>
2016-06-21 13:58:38 -0700

    Replace toast in mFindRestaurantButton click listener with intent to travel from MainActivity to RestaurantsActivity.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
