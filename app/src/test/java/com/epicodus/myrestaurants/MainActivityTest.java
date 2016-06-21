04ebb04dc044b97adb8fbd11e0713710d2d4e1ac
Courtney <courtney@epicodus.com>
2016-06-21 15:02:24 -0700

    Add test using shadows to confirm Intent created in MainActivity successfully navigates to RestaurantsActivity.

--- app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.epicodus.myrestaurants;
 
+import android.content.Intent;
 import android.os.Build;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -27,5 +29,14 @@ public class MainActivityTest {
     public void validateTextViewContent() {
         TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
         assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.findRestaurantsButton).performClick();
+        Intent expectedIntent = new Intent(activity, RestaurantsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
