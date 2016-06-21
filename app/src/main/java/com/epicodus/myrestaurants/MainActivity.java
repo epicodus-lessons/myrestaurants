e95efcdd46bdd3d312718ee9165af7beac585aa2
Courtney <courtney@epicodus.com>
2016-06-21 11:46:46 -0700

    Declare and import mFindRestaurantsButton Button in MainActivity.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -2,8 +2,10 @@ package com.epicodus.myrestaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
