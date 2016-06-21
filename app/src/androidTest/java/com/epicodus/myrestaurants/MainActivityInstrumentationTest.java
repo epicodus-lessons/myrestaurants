991a6438ffba5b4df60ca31f9af5c29000081181
Courtney <courtney@epicodus.com>
2016-06-21 15:20:25 -0700

    Write instrumentation test to validate text entered into locationEditText is visible in layout. Import all necessary static methods.

--- app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -1,4 +1,26 @@
 package com.epicodus.myrestaurants;
 
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
+                .check(matches(withText("Portland")));
+    }
+
 }
