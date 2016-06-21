6e536155b17f06c1404c4de38b10137c2577559a
Courtney <courtney@epicodus.com>
2016-06-21 15:25:51 -0700

    Include test to ensure user input in locationEditText field on MainActivity is successfully being passed to RestaurantsActivity via an Intent extra.

--- app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -6,6 +6,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -21,6 +22,15 @@ public class MainActivityInstrumentationTest {
     public void validateEditText() {
         onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
                 .check(matches(withText("Portland")));
+    }
+
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String location = "Portland";
+        onView(withId(R.id.locationEditText)).perform(typeText(location));
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("Here are all the restaurants near: " + location)));
     }
 
 }
