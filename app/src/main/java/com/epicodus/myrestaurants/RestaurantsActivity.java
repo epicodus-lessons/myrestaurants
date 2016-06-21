4af8f812770da60c6fa7d44a1c56fde8a9e8d15e
Courtney <courtney@epicodus.com>
2016-06-21 13:57:06 -0700

    Create new RestaurantsActivity and corresponding layout file.

---
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.myrestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
