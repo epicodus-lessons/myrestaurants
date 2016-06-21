d543f7ab2eb59f903398b78b830974397b42220d
Courtney <courtney@epicodus.com>
2016-06-21 15:00:45 -0700

    Include passing test asserting that MainActivity TextView correctly reads 'MyRestaurants'.

--- app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
@@ -1,12 +1,16 @@
 package com.epicodus.myrestaurants;
 
 import android.os.Build;
+import android.widget.TextView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
 
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricGradleTestRunner.class)
@@ -17,5 +21,11 @@ public class MainActivityTest {
     @Before
     public void setup() {
         activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
     }
 }
