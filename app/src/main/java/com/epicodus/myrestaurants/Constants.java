960d46acb076da8464b346d7779a5d12ec1c1fba
Courtney <courtney@epicodus.com>
2016-06-22 16:24:50 -0700

    Add values placed in gradle.properties to BuildConfig file at runtime, so Constants class may access Yelp API credentials.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -1,4 +1,8 @@
 package com.epicodus.myrestaurants;
 
 public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
 }
