b06745549646b31545116521b07950fc8b9f7e13
Courtney <courtney@epicodus.com>
2016-06-22 11:14:54 -0700

    Create rule to begin RestaurantsActivity before each test, write basic test to assert correct number of restaurants are being displayed in ListView.

--- app/src/test/java/com/epicodus/myrestaurants/RestaurantsActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/RestaurantsActivityTest.java
@@ -1,7 +1,35 @@
 package com.epicodus.myrestaurants;
 
-/**
- * Created by staff on 6/22/16.
- */
+import android.os.Build;
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
 public class RestaurantsActivityTest {
+    private RestaurantsActivity activity;
+    private ListView mRestaurantListView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(RestaurantsActivity.class);
+        mRestaurantListView = (ListView) activity.findViewById(R.id.listView);
+    }
+
+    @Test
+    public void restaurantListViewPopulates() {
+        assertNotNull(mRestaurantListView.getAdapter());
+        assertEquals(mRestaurantListView.getAdapter().getCount(), 15);
+    }
+
 }
