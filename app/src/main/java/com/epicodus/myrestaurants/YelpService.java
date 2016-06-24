87b5d2b29b725177c74d735fc975ab501cbb39c0
Courtney <courtney@epicodus.com>
2016-06-24 14:34:29 -0700

    Create findRestaurants() method in YelpService class to manage creating and signing OAuth requests with OkHttp and SignPost.

--- app/src/main/java/com/epicodus/myrestaurants/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpService.java
@@ -1,7 +1,33 @@
 package com.epicodus.myrestaurants;
 
-/**
- * Created by staff on 6/24/16.
- */
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
 public class YelpService {
+
+    public static void findRestaurants(String location, Callback callback) {
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
+        consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new SigningInterceptor(consumer))
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
 }
