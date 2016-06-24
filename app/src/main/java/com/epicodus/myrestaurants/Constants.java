a4deac932e8c06c0ccad6a3ed6f8e8dcddef9ca9
Courtney <courtney@epicodus.com>
2016-06-24 14:31:50 -0700

    Add Yelp API base URL and query parameters to Constants class.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
     public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 }
