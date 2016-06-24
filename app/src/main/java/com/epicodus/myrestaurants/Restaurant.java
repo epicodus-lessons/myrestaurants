1645d961bb94915a231496066d8ea1e486d2cc74
Courtney <courtney@epicodus.com>
2016-06-24 14:44:38 -0700

    Define all necessary member variables, getters, and setters for all Restaurant model attributes.

--- app/src/main/java/com/epicodus/myrestaurants/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/Restaurant.java
@@ -1,4 +1,65 @@
 package com.epicodus.myrestaurants;
 
+import java.util.ArrayList;
+
 public class Restaurant {
+    private String mName;
+    private String mPhone;
+    private String mWebsite;
+    private double mRating;
+    private String mImageUrl;
+    private ArrayList<String> mAddress = new ArrayList<>();
+    private double mLatitude;
+    private double mLongitude;
+    private ArrayList<String> mCategories = new ArrayList<>();
+
+    public Restaurant(String name, String phone, String website,
+                      double rating, String imageUrl, ArrayList<String> address,
+                      double latitude, double longitude, ArrayList<String> categories) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mCategories = categories;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public ArrayList<String> getCategories() {
+        return mCategories;
+    }
 }
