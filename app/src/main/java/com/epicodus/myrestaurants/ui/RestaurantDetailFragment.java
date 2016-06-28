5bfb40a7f524f1a670e15cf5783dc77c99cbf823
Courtney <courtney@epicodus.com>
2016-06-27 18:58:28 -0700

    Create new RestaurantDetailFragment to display individual restaurant details.

---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
@@ -0,0 +1,25 @@
+package com.epicodus.myrestaurants.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.myrestaurants.R;
+
+public class RestaurantDetailFragment extends Fragment {
+
+    public RestaurantDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+    }
+
+}
