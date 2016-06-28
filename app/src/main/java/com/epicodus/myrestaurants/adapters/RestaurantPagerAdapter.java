b6fb19ab991bb0a99ed530e12f4445daf0caf6ee
Courtney <courtney@epicodus.com>
2016-06-28 12:28:47 -0700

    Populate RestaurantPagerAdapter with code to set FragmentManager, create new instances of RestaurantDetailFragment, calculate how many fragments are necessary, and update title tabs.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantPagerAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantPagerAdapter.java
@@ -1,4 +1,35 @@
 package com.epicodus.myrestaurants.adapters;
 
-public class RestaurantPagerAdapter {
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.ui.RestaurantDetailFragment;
+
+import java.util.ArrayList;
+
+public class RestaurantPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Restaurant> mRestaurants;
+
+    public RestaurantPagerAdapter(FragmentManager fm, ArrayList<Restaurant> restaurants) {
+        super(fm);
+        mRestaurants = restaurants;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return RestaurantDetailFragment.newInstance(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mRestaurants.get(position).getName();
+    }
 }
