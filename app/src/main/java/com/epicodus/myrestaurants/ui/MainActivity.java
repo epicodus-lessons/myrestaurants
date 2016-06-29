6c7731dafb4d2c906d5c89e6704c9c1d983e8a19
Courtney <courtney@epicodus.com>
2016-06-29 13:00:08 -0700

    Add .push() to saveLocationToFirebase() method to save locations to database under unique ID.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void saveLocationToFirebase(String location) {
-        mSearchedLocationReference.setValue(location);
+        mSearchedLocationReference.push().setValue(location);
     }
 
 //    private void addToSharedPreferences(String location) {
