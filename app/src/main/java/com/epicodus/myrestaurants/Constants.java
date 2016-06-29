4aa300f30b80cd8a46d57754e503c9e59189c716
Courtney <courtney@epicodus.com>
2016-06-29 12:14:41 -0700

    Add searchedLocation constant to Constants class to function as the key in the key-value pair used to save user-entered zip codes into the Firebase database.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
     public static final String PREFERENCES_LOCATION_KEY = "location";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
 }
