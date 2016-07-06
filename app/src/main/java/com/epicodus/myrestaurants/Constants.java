3deb49d122238bdbb1ee321779d2b6a63a4ed9e5
Courtney <courtney@epicodus.com>
2016-07-05 18:06:42 -0700

    Add restaurants key to Constants.java class to act as key in key-value relationship for all restaurants saved to Firebase.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
     public static final String PREFERENCES_LOCATION_KEY = "location";
     public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
+    public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants";
 }
