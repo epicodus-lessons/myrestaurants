3c2b43006111060be17429d054e01b1f97e278dd
Courtney <courtney@epicodus.com>
2016-07-06 18:06:33 -0700

    Populate new FirebaseRestaurantViewHolder class with code to handle binding views, resizing images, creating click listeners, creating intents, etc.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -1,4 +1,82 @@
 package com.epicodus.myrestaurants.adapters;
 
-public class FirebaseRestaurantViewHolder {
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.epicodus.myrestaurants.Constants;
+import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private static final int MAX_WIDTH = 200;
+    private static final int MAX_HEIGHT = 200;
+
+    View mView;
+    Context mContext;
+
+    public FirebaseRestaurantViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindRestaurant(Restaurant restaurant) {
+        ImageView restaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
+        TextView nameTextView = (TextView) mView.findViewById(R.id.restaurantNameTextView);
+        TextView categoryTextView = (TextView) mView.findViewById(R.id.categoryTextView);
+        TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
+
+        Picasso.with(mContext)
+                .load(restaurant.getImageUrl())
+                .resize(MAX_WIDTH, MAX_HEIGHT)
+                .centerCrop()
+                .into(restaurantImageView);
+
+        nameTextView.setText(restaurant.getName());
+        categoryTextView.setText(restaurant.getCategories().get(0));
+        ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Restaurant> restaurants = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    restaurants.add(snapshot.getValue(Restaurant.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("restaurants", Parcels.wrap(restaurants));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
 }
