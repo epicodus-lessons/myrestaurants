d9029b997740e525a92911c2c5d7c1867d5a12f6
Courtney <courtney@epicodus.com>
2016-07-06 19:40:23 -0700

    Update Google Play Services SDK.

--- app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
@@ -3,6 +3,8 @@ package com.epicodus.myrestaurants.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.epicodus.myrestaurants.R;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
