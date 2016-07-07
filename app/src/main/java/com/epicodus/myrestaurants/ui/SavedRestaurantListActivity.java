e01b07be1d07a610924c901e445ed2420938c6a4
Courtney <courtney@epicodus.com>
2016-07-06 18:07:46 -0700

    Create new SavedRestaurantListActivity to handle displaying a user's list of restaurants they've saved.

---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -0,0 +1,12 @@
+package com.epicodus.myrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedRestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
