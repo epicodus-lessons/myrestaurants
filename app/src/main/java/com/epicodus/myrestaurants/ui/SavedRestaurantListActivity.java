b62ccddb030e01175b38f057ece17a6905a8c2ea
Courtney <courtney@epicodus.com>
2016-07-06 18:09:34 -0700

    Populate SavedRestaurantListActivity wiht code to handle connecting to Firebase, setting content in the activity_restaurants layout, populating view holders, binding restaurants, and cleaning up when activity is destroyed.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -2,11 +2,56 @@ package com.epicodus.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.epicodus.myrestaurants.Constants;
+import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SavedRestaurantListActivity extends AppCompatActivity {
+    private DatabaseReference mRestaurantReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
+
+        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder>
+                (Restaurant.class, R.layout.restaurant_list_item, FirebaseRestaurantViewHolder.class,
+                        mRestaurantReference) {
+
+            @Override
+            protected void populateViewHolder(FirebaseRestaurantViewHolder viewHolder,
+                                              Restaurant model, int position) {
+                viewHolder.bindRestaurant(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
