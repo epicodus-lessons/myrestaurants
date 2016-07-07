df176eb625a1ac7b31418d44e5714e4b367a8cf5
Courtney <courtney@epicodus.com>
2016-07-06 19:25:20 -0700

    Create new CreateAccountActivity to allow users to register for new accounts.

---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.myrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
