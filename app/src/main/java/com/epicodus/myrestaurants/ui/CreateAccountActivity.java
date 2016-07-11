f2c33b0f4f6bfe3cff82af22606f9fb8865ffa65
Courtney <courtney@epicodus.com>
2016-07-11 10:07:27 -0700

    Bind all views in CreateAccountActivity with ButterKnife.

--- app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
@@ -2,14 +2,29 @@ package com.epicodus.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.epicodus.myrestaurants.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class CreateAccountActivity extends AppCompatActivity {
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+
     }
 }
