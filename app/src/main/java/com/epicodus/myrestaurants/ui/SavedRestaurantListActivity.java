277892252e28ad0f46e1dd137202d76a9de56513
Courtney <courtney@epicodus.com>
2016-07-12 14:54:33 -0700

    Retrieve UID from Firebase in onCreate() lifecycle method of SavedRestaurantListActivity to display the list of saved restaurants specifically assocated with that user's account in Firebase.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -10,6 +10,8 @@ import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -28,6 +30,9 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
 
         mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
         setUpFirebaseAdapter();
