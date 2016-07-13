dd9f1d1731e8d350dcc2138a66a878dee91c619c
Courtney <courtney@epicodus.com>
2016-07-13 10:45:33 -0700

    Set OnTouchListener in populateViewHolder() method of FirebaseRestaurantListAdapter in order to listen for drag events, and respond appropriately when they occur.

--- app/src/main/java/com/epicodus/myrestaurants/util/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/FirebaseRestaurantListAdapter.java
@@ -1,6 +1,9 @@
 package com.epicodus.myrestaurants.util;
 
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
 import com.epicodus.myrestaurants.models.Restaurant;
@@ -23,8 +26,20 @@ public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Resta
     }
 
     @Override
-    protected void populateViewHolder(FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position) {
+    protected void populateViewHolder(final FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position) {
         viewHolder.bindRestaurant(model);
+
+        viewHolder.mRestaurantImageView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    mOnStartDragListener.onStartDrag(viewHolder);
+                }
+                return false;
+            }
+
+        });
     }
 
     @Override
