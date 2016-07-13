00561f98f868dfcdb30c4dceb7c65fb8605a2c9b
Courtney <courtney@epicodus.com>
2016-07-13 10:31:26 -0700

    Implement OnStartDragListener interface in SavedRestaurantListActivity, refactor onCreate() and setUpFirebaseAdapter() methods to implement ItemTouchHelper in order to enable gesture features.

--- app/src/main/java/com/epicodus/myrestaurants/util/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/FirebaseRestaurantListAdapter.java
@@ -1,4 +1,39 @@
 package com.epicodus.myrestaurants.util;
 
-public class FirebaseRestaurantListAdapter {
+import android.content.Context;
+
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder> implements ItemTouchHelperAdapter {
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+    public FirebaseRestaurantListAdapter(Class<Restaurant> modelClass, int modelLayout,
+                                         Class<FirebaseRestaurantViewHolder> viewHolderClass,
+                                         Query ref, OnStartDragListener onStartDragListener, Context context) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        mRef = ref.getRef();
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
+
+    @Override
+    protected void populateViewHolder(FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position) {
+        viewHolder.bindRestaurant(model);
+    }
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        return false;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+
+    }
 }
