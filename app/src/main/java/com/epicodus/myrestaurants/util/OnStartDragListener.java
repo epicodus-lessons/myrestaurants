0337cb1b5270f52d714bea16e784feaef86c5650
Courtney <courtney@epicodus.com>
2016-07-13 09:56:56 -0700

    Add onStartDrag() method to OnStartDragListener interface to eventually handle drag-and-drop interactions.

--- app/src/main/java/com/epicodus/myrestaurants/util/OnStartDragListener.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/OnStartDragListener.java
@@ -1,4 +1,7 @@
 package com.epicodus.myrestaurants.util;
 
+import android.support.v7.widget.RecyclerView;
+
 public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
