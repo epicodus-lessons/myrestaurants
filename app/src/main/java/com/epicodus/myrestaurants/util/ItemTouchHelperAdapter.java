d06f8d884a3b4e5e7497d52ec3838357cc9bacf5
Courtney <courtney@epicodus.com>
2016-07-13 09:55:56 -0700

    Define ItemTouchHelperAdapter interface with two methods: onItemMove() and onItemDismiss().

--- app/src/main/java/com/epicodus/myrestaurants/util/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/ItemTouchHelperAdapter.java
@@ -1,4 +1,6 @@
 package com.epicodus.myrestaurants.util;
 
-public class ItemTouchHelperAdapter {
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
 }
