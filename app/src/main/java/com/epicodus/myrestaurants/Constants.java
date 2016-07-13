f759f6cafeb95d5bcc17fc9b9b06a61b8080a681
Courtney <courtney@epicodus.com>
2016-07-13 10:50:10 -0700

    Add index key to Constants class.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
     public static final String PREFERENCES_LOCATION_KEY = "location";
     public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
     public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants";
+    public static final String FIREBASE_QUERY_INDEX = "index";
+
 }
