b6f7f61789b09d119d3ee82113a30cefd1383a2c
Courtney <courtney@epicodus.com>
2016-07-18 13:09:16 -0700

    Remove import statements no longer in use from multiple files.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -20,11 +20,6 @@ import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -2,24 +2,8 @@ package com.epicodus.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-
-import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
-import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
-import com.epicodus.myrestaurants.models.Restaurant;
-import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
-import com.epicodus.myrestaurants.util.OnStartDragListener;
-import com.epicodus.myrestaurants.util.SimpleItemTouchHelperCallback;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 public class SavedRestaurantListActivity extends AppCompatActivity {
 
