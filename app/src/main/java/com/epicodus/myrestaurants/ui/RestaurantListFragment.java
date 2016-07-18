4ae12b56678a8976e49414e5cc59c7d3fbfa9bf2
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-07-18 10:38:42 -0700

    Create RestaurantListFragment for eventual use with Alternate Resources.

---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
@@ -0,0 +1,27 @@
+package com.epicodus.myrestaurants.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.myrestaurants.R;
+
+public class RestaurantListFragment extends Fragment {
+
+
+    public RestaurantListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant_list, container, false);
+    }
+
+}
