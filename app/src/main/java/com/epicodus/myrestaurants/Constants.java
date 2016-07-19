b7436049fa1f6e555e99a50dd08428a51de14e7f
Courtney <courtney@epicodus.com>
2016-07-18 17:48:38 -0700

    Add EXTRA_KEY_POSITION and EXTRA_KEY_RESTAURANTS constants to Constants.java class for use in RestaurantListAdapter's onClick() override.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -11,5 +11,7 @@ public class Constants {
     public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
     public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants";
     public static final String FIREBASE_QUERY_INDEX = "index";
+    public static final String EXTRA_KEY_POSITION = "position";
+    public static final String EXTRA_KEY_RESTAURANTS = "restaurants";
 
 }
