68f5881d6ba4119d642d6035b5a4c510e43f0bbf
Courtney <courtney@epicodus.com>
2016-07-19 16:13:48 -0700

    Add keys to Constants.java class.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -13,5 +13,8 @@ public class Constants {
     public static final String FIREBASE_QUERY_INDEX = "index";
     public static final String EXTRA_KEY_POSITION = "position";
     public static final String EXTRA_KEY_RESTAURANTS = "restaurants";
+    public static final String KEY_SOURCE = "source";
+    public static final String SOURCE_SAVED = "saved";
+    public static final String SOURCE_FIND = "find";
 
 }
