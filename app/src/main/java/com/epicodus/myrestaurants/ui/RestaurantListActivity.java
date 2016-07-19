59e4f30e70685ecf4106eabf44184b93953356ed
Courtney <courtney@epicodus.com>
2016-07-19 09:25:49 -0700

    Implement OnRestaurantSelectedListener and override its methdos in RestaurantListActivity.

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
@@ -3,14 +3,26 @@ package com.epicodus.myrestaurants.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.OnRestaurantSelectedListener;
+
+import java.util.ArrayList;
 
 
-public class RestaurantListActivity extends AppCompatActivity {
+public class RestaurantListActivity extends AppCompatActivity implements OnRestaurantSelectedListener {
+    private Integer mPosition;
+    ArrayList<Restaurant> mRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+    }
+
+    @Override
+    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants) {
+        mPosition = position;
+        mRestaurants = restaurants;
     }
 
 }
