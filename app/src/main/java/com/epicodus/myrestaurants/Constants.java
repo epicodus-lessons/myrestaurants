48bfba1f6fd39cb0b21121a86e7c4daeabc61fb0
Courtney Phillips <courtney@epicodus.com>
2017-10-12 16:20:35 -0700

    Update buildTypes in build.gradle to include Yelp token.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -1,4 +1,5 @@
 package com.epicodus.myrestaurants;
 
-public class Constants {public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
 }
