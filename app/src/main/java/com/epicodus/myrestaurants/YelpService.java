3418680092d328d3e1c1f6551dbb6f6a569c5319
Courtney Phillips <courtney@epicodus.com>
2017-10-13 11:23:20 -0700

    Create new instance of OkHttp client in findRestaurants() method.

--- app/src/main/java/com/epicodus/myrestaurants/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpService.java
@@ -1,11 +1,14 @@
 package com.epicodus.myrestaurants;
 
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 
 public class YelpService {
 
     public static void findRestaurants(String location, Callback callback) {
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
     }
 
 }
