4b1dc53cfd3b4ffcdb904bb6cc827aca32f12e7b
Courtney Phillips <courtney@epicodus.com>
2017-10-13 11:08:17 -0700

    Declare findRestaurants() method in YelpService to manage API call.

--- app/src/main/java/com/epicodus/myrestaurants/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpService.java
@@ -1,4 +1,11 @@
 package com.epicodus.myrestaurants;
 
+import okhttp3.Callback;
+
 public class YelpService {
+
+    public static void findRestaurants(String location, Callback callback) {
+
+    }
+
 }
