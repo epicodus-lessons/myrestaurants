48695ab4644259518a2300c721a775ddb7d40f77
Courtney Phillips <courtney@epicodus.com>
2017-10-13 13:24:59 -0700

    Add methods required by RecyclerView.Adapter: onCreateViewHolder, onBindViewHolder, and getItemCount.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
@@ -3,7 +3,9 @@ package com.epicodus.myrestaurants.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,6 +25,24 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         mContext = context;
         mRestaurants = restaurants;
     }
+
+    @Override
+    public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_list_item, parent, false);
+        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RestaurantListAdapter.RestaurantViewHolder holder, int position) {
+        holder.bindRestaurant(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRestaurants.size();
+    }
+
 
     public class RestaurantViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.restaurantImageView)
