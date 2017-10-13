d74298afc78358864fe2be7bde6cc7b2244bc3c3
Courtney Phillips <courtney@epicodus.com>
2017-10-13 11:34:32 -0700

    Fix error in base URL constant.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -2,8 +2,8 @@ package com.epicodus.myrestaurants;
 
 public class Constants {
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurants";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
-    public static final String API_KEY_QUERY_PARAMETER = "Authorization";
-    public static final String PREFERENCES_LOCATION_KEY = "city";
+//    public static final String API_KEY_QUERY_PARAMETER = "Authorization";
+//    public static final String PREFERENCES_LOCATION_KEY = "city";
 }
