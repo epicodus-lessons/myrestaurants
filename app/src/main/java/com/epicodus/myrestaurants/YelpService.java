f0604a81ad2c29814a89933a484c7f946bc2ab7a
Courtney Phillips <courtney@epicodus.com>
2017-10-13 11:24:16 -0700

    Build and call new OkHttp API request with necessary parameters.

--- app/src/main/java/com/epicodus/myrestaurants/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpService.java
@@ -1,8 +1,10 @@
 package com.epicodus.myrestaurants;
 
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class YelpService {
 
@@ -14,6 +16,14 @@ public class YelpService {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.YELP_TOKEN)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
 
     }
 
