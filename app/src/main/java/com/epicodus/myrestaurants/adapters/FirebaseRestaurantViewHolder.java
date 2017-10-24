d327a15a64ddf43b32dcb07bf055c63c29ee8626
Courtney Phillips <courtney@epicodus.com>
2017-10-24 16:51:30 -0700

    Add logic to decode images retrieved from Firebase in FirebaseRestaurantViewHolder.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -3,8 +3,10 @@ package com.epicodus.myrestaurants.adapters;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +16,10 @@ import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.util.ItemTouchHelperViewHolder;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+
+import static com.epicodus.myrestaurants.R.id.categoryTextView;
+import static com.epicodus.myrestaurants.R.id.ratingTextView;
 import static com.epicodus.myrestaurants.R.id.restaurantImageView;
 
 public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
@@ -32,19 +38,30 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
 
     public void bindRestaurant(Restaurant restaurant) {
         mRestaurantImageView = (ImageView) mView.findViewById(restaurantImageView);
-        TextView nameTextView = (TextView) mView.findViewById(R.id.restaurantNameTextView);
-        TextView categoryTextView = (TextView) mView.findViewById(R.id.categoryTextView);
-        TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
+        TextView mNameTextView = (TextView) mView.findViewById(R.id.restaurantNameTextView);
+        TextView mCategoryTextView = (TextView) mView.findViewById(categoryTextView);
+        TextView mRatingTextView = (TextView) mView.findViewById(ratingTextView);
 
-        Picasso.with(mContext)
-                .load(restaurant.getImageUrl())
-                .resize(MAX_WIDTH, MAX_HEIGHT)
-                .centerCrop()
-                .into(mRestaurantImageView);
-
-        nameTextView.setText(restaurant.getName());
-        categoryTextView.setText(restaurant.getCategories().get(0));
-        ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        if (!restaurant.getImageUrl().contains("http")) {
+            try {
+                Bitmap imageBitmap = decodeFromFirebaseBase64(restaurant.getImageUrl());
+                mRestaurantImageView.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Picasso.with(mContext)
+                    .load(restaurant.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(mRestaurantImageView);
+            mNameTextView.setText(restaurant.getName());
+            mCategoryTextView.setText(restaurant.getCategories().get(0));
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
+        mNameTextView.setText(restaurant.getName());
+        mCategoryTextView.setText(restaurant.getCategories().get(0));
+        mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
     }
 
     @Override
@@ -61,6 +78,11 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
                 R.animator.drag_scale_off);
         set.setTarget(itemView);
         set.start();
+    }
+
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
     }
 
 }
