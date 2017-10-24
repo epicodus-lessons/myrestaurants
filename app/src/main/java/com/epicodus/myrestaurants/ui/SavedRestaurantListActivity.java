8d805e9e89a10f3a8b2fb48056dadc75db95871a
Courtney Phillips <courtney@epicodus.com>
2017-10-24 14:47:04 -0700

    Remove all methods and references to methods from SavedRestaurantListActivity that have been moved to SavedRestaurantListFragment.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -2,79 +2,15 @@ package com.epicodus.myrestaurants.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 
-import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
-import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
-import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
-import com.epicodus.myrestaurants.models.Restaurant;
-import com.epicodus.myrestaurants.util.OnStartDragListener;
-import com.epicodus.myrestaurants.util.SimpleItemTouchHelperCallback;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
-public class SavedRestaurantListActivity extends AppCompatActivity implements OnStartDragListener {
-    private DatabaseReference mRestaurantReference;
-//    private FirebaseRestaurantListAdapter mFirebaseAdapter;
-//    private ItemTouchHelper mItemTouchHelper;
-//
-//    @Bind(R.id.recyclerView)
-//    RecyclerView mRecyclerView;
+public class SavedRestaurantListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_restaurants);
-        ButterKnife.bind(this);
-
-//        setUpFirebaseAdapter();
     }
 
-//    private void setUpFirebaseAdapter() {
-//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//        String uid = user.getUid();
-//
-//        Query query = FirebaseDatabase.getInstance()
-//                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
-//                .child(uid)
-//                .orderByChild(Constants.FIREBASE_QUERY_INDEX);
-//
-//        mRestaurantReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
-//                .child(uid);
-//
-//        mFirebaseAdapter = new FirebaseRestaurantListAdapter(Restaurant.class,
-//                R.layout.restaurant_list_item_drag, FirebaseRestaurantViewHolder.class,
-//                mRestaurantReference, this, this);
-//
-//        mRecyclerView.setHasFixedSize(true);
-//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        mRecyclerView.setAdapter(mFirebaseAdapter);
-//
-//        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
-//        mItemTouchHelper = new ItemTouchHelper(callback);
-//        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
-//    }
-
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mFirebaseAdapter.cleanup();
-//    }
-//
-//    @Override
-//    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
-//        mItemTouchHelper.startDrag(viewHolder);
-//    }
 }
