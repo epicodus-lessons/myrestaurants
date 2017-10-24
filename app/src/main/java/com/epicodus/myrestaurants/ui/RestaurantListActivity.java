1266053ce9146e69af2591bd7b93961d52754da1
Courtney Phillips <courtney@epicodus.com>
2017-10-24 15:34:58 -0700

    Implement new onRestaurantSelectedListener interface in RestaurantListActivity, override sole method.

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
@@ -4,13 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.OnRestaurantSelectedListener;
 
-public class RestaurantListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RestaurantListActivity extends AppCompatActivity implements OnRestaurantSelectedListener {
+    private Integer mPosition;
+    ArrayList<Restaurant> mRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+    }
+
+    @Override
+    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants) {
+        mPosition = position;
+        mRestaurants = restaurants;
     }
 
 }
