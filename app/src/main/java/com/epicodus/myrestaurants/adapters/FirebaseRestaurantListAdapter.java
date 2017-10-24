73e1105df88db0cd03df254b6cf0a57adb3b3e44
Courtney Phillips <courtney@epicodus.com>
2017-10-24 12:42:51 -0700

    Notify FirebaseRestaurantListAdapter of drag-and-drop movement in ItemTouchHelperAdapter overrides.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -44,11 +44,13 @@ public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Resta
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
+        notifyItemMoved(fromPosition, toPosition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
-
+        getRef(position).removeValue();
     }
+
 }
